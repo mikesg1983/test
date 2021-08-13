b3e37adba7f2fe0956d84ff054c6dbf94f8836c6
mikesg1983 <88478361+mikesg1983@users.noreply.github.com>
2021-08-13 09:53:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
-
-provider "google" {
-  #credentials = file("/home/mxsystem/learn-terraform-gcp/sa-vpc-build.json")
-  #credentials = file("https://storage.cloud.google.com/quantum-device-322319-tfstate/jenkins/sa-vpc-build.json")
-
-  project = "quantum-device-322319"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
